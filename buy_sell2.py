9b0dd475d78cff4f620592e0398f859e04655f46
RicardoRodriguesCosta <ricardodouglasrodrigues@gmail.com>
2021-01-08 16:34:29 -0300

    Justa a set of pseudo astrolabes to help me.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   AAPL.csv
    	new file:   buy_sell.py
    	new file:   buy_sell2.py
    	new file:   stocks.txt
    	new file:   test.csv
    	new file:   twelvdatascript.py

---
+++ buy_sell2.py
@@ -0,0 +1,106 @@
+from tradingview_ta import TA_Handler, Interval
+import subprocess
+from terminaltables import AsciiTable
+
+def loop_simples_de_coleta(argumento,errormsg,classe):
+	if classe == "int":
+		while True:
+			try:
+				resposta = int(input(argumento))
+				return resposta
+			except:
+				print(errormsg)
+	elif classe == "str":
+		while True:
+			try:
+				resposta = input(argumento)
+				return resposta
+			except:
+				print(errormsg)
+	elif classe == "bool":
+		while True:
+			try:
+				resposta = bool(int(input(argumento)))
+				return resposta
+			except:
+				print(errormsg)
+	elif classe == "float":
+		while True:
+			try:
+				resposta = float(input(argumento))
+				return resposta
+			except:
+				print(errormsg)
+
+
+config = ["Try once again\n","int","str","Stock Symbol:     ","Market Symbol:    ","Time Interval:    "]
+
+def get_stock():
+    handler = TA_Handler()
+    symbol = loop_simples_de_coleta(config[3],config[0],config[2])
+    handler.set_symbol_as(symbol)
+    exchange = loop_simples_de_coleta(config[4],config[0],config[2])
+    handler.set_exchange_as_crypto_or_stock(exchange)
+    handler.set_screener_as_stock("america")
+    r ="Interval." + str(interval_options())
+    handler.set_interval_as(r)
+    stock = handler.get_analysis().summary
+    stock_table = []
+    for i in stock.items():
+        stock_table.append(i)
+    stock_table.insert(0,["Company: " + symbol.upper(),"Result"])
+    stock_table = AsciiTable(stock_table)
+    print(stock_table.table)
+
+def receive_input(lista=list,valor=int):
+    for i in lista:
+        if i[-1] == valor:
+            return i[0]
+        else:
+            continue
+
+def get_list_stock(lista=list):
+    handler = TA_Handler()
+    for i in range(len(lista)):
+        handler.set_symbol_as(lista[i][0])
+        handler.set_exchange_as_crypto_or_stock("NASDAQ")
+        handler.set_screener_as_stock("america")
+        handler.set_interval_as(Interval.INTERVAL_1_HOUR)
+        stock = handler.get_analysis().summary
+        stock_table = []
+        for b in stock.items():
+            stock_table.append(b)
+        stock_table.insert(0,["Company: " + lista[i][0].upper(),"Result"])
+        #print(lista[i])
+        stock_table = AsciiTable(stock_table)
+        print(stock_table.table)
+
+
+def stock_pref_list():
+    stock_pref_listado = []
+    with open("stocks.txt","r") as stock_list:
+        for stock in stock_list:
+            #print(stock)
+            stock_pref_listado.append(stock.split())
+    return stock_pref_listado
+
+def interval_options():#try to implement some get function to auto populate the list.
+    interval_options = [["Option", "#"],[Interval.INTERVAL_1_MINUTE,1],
+ [Interval.INTERVAL_5_MINUTES,2],
+ [Interval.INTERVAL_15_MINUTES,3],
+ [Interval.INTERVAL_1_HOUR,4],
+ [Interval.INTERVAL_4_HOURS,5],
+ [Interval.INTERVAL_1_DAY,6],
+ [Interval.INTERVAL_1_WEEK,7],
+ [Interval.INTERVAL_1_MONTH,8],["Voltar",0]]
+
+    table_interval = AsciiTable(interval_options)
+    print(table_interval.table)
+    interval_resp = loop_simples_de_coleta(config[5],config[0],config[2])
+    r = receive_input(interval_options,interval_resp)
+    return r
+
+if __name__ == "__main__":
+    #stock_name = ["TSLA","AAPL","GOOGL","AMD"]
+    print(stock_pref_list())
+    get_list_stock(stock_pref_list())
---
+++ twelvdatascript.py
@@ -0,0 +1,32 @@
+from twelvedata import TDClient
+import pandas as pd
+import matplotlib.pyplot as plt 
+import mplfinance as mpf
+import pandas as pd 
+import matplotlib.dates as mpdates
+
+def update_stock():
+    td = TDClient(apikey="0d3992e7cb304ed4a384ccc468d35dfc")
+    ts = td.time_series(symbol="AAPL",interval="1day",timezone="America/Argentina/Buenos_Aires",start_date="2020-11-02",end_date="2021-01-07",outputsize=5000).as_pandas()
+    ts = ts.sort_values("datetime")#I Spend too much time sorting the DataFrame and not saving it hahaha
+    print(ts)
+    ts.to_csv("AAPL.csv")
+
+ts = pd.read_csv("AAPL.csv",parse_dates=True)
+print(ts)
+print(type(ts))
+"""
+Use existing date column as index:
+https://queirozf.com/entries/pandas-time-series-examples-datetimeindex-periodindex-and-timedeltaindex
+"""
+def to_data_index(ts):
+    datetime_series = pd.to_datetime(ts['datetime'])
+    datetime_index = pd.DatetimeIndex(datetime_series.values)
+    tss=ts.set_index(datetime_index)
+    tss.drop('datetime',axis=1,inplace=True)
+    return tss
+tss = to_data_index(ts)
+print(tss)
+mpf.plot(tss,volume=True,type="candle",mav=4)
+#update_stock()
+
